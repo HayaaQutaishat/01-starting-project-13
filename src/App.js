080637aa4679e9eb1b4e7a65b598abde65e6f2f1
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-06 15:28:20 +0100

    Handle the response and potential errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { uiActions } from "./store/ui";
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.showCart);
   // latest cart state
   const cart = useSelector((state) => state.cart);
 
   // every time cart state changes, send HTTP request to the backend
   useEffect(() => {
-    fetch(
-      "https://react-http-489ae-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotifications({
+          title: "pending",
+          status: "pending",
+          message: "Sedning Cart Data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-489ae-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
       }
-    );
-  }, [cart]);
+      dispatch(
+        uiActions.showNotifications({
+          status: "success",
+          title: "success",
+          message: "Sent Cart Data Successfully!",
+        })
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotifications({
+          status: "error",
+          title: "error",
+          message: "Sending Cart Data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
   return (
     <Layout>
       {showCart && <Cart />}
