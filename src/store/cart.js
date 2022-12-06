55211615769b1273172fa8b06e38ae9943594b91
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-06 12:17:36 +0100

    Adding the logic of the reducers methods

--- src/store/cart.js
+++ src/store/cart.js
@@ -9,8 +9,38 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: initialCartState,
   reducers: {
-    addItemToCart(state, action) {},
-    removeItemFromCart(state, action) {},
+    addItemToCart(state, action) {
+      const new_item = action.payload;
+      state.totalQuantity++;
+      //   check if the item is already in the cart
+      const existingItem = state.items.find((item) => item.id === new_item.id);
+      if (!existingItem) {
+        // add the item to the list
+        state.items.push({
+          id: new_item.id,
+          title: new_item.title,
+          price: new_item.price,
+          quantity: 1,
+          totalPrice: new_item.price,
+        });
+      } else {
+        // update the existing item
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + new_item.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        // remove the item from the array entirely
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        // reduce quantity by 1
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
