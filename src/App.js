bf69949becb8a34fac591934f55f7d5ece95fc27
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-06 17:21:45 +0100

    Using Action Creator Thunk Alternative

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui";
 import React from "react";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart";
 
 let isInitial = true;
 
@@ -18,47 +18,12 @@ function App() {
 
   // every time cart state changes, send HTTP request to the backend
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotifications({
-          title: "pending",
-          status: "pending",
-          message: "Sedning Cart Data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-489ae-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed!");
-      }
-      dispatch(
-        uiActions.showNotifications({
-          status: "success",
-          title: "success",
-          message: "Sent Cart Data Successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotifications({
-          status: "error",
-          title: "error",
-          message: "Sending Cart Data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <React.Fragment>
