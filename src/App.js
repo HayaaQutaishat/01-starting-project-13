a54b22387387f67ae06a4bc2b0d8522fff1b5ed0
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-06 14:41:52 +0100

    Send the Updated Cart to backend(Firebase)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.showCart);
+  // latest cart state
+  const cart = useSelector((state) => state.cart);
+
+  // every time cart state changes, send HTTP request to the backend
+  useEffect(() => {
+    fetch(
+      "https://react-http-489ae-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
