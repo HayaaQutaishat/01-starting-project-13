aa76ce4742c1ea88fd8a5f08551740aae9e9bdf5
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-07 17:18:26 +0100

    Creat action creator to fetch data from Firebase

--- src/store/cart-action-creator.js
+++ src/store/cart-action-creator.js
@@ -1,5 +1,39 @@
 import { uiActions } from "./ui";
 
+// create action creator (to fetch data from firebase)
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotifications({
+        title: "pending",
+        status: "pending",
+        message: "Fetching Cart Data!",
+      })
+    );
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-http-489ae-default-rtdb.europe-west1.firebasedatabase.app/cart.json"
+      );
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+      const data = await response.json();
+      return data;
+    };
+    try {
+      const cartData = await fetchData();
+    } catch (error) {
+      dispatch(
+        uiActions.showNotifications({
+          title: "error",
+          status: "error",
+          message: "Fetching Cart Data Failed!",
+        })
+      );
+    }
+  };
+};
+
 // create custom action creator (to send data)
 export const sendCartData = (cart) => {
   return async (dispatch) => {
